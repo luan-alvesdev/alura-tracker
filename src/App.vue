74b7cc370b24e568302f43323ca9356b77603bba
luan-alvesdev <lluan.alves1@gmail.com>
2023-07-25 18:37:50 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="colum is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="colum is-three-quarter">
+      <FormularioTarefa />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue'
+import FormularioTarefa from './components/FormularioTarefa.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    FormularioTarefa
+  }
+});
+</script>
+
+<style>
+
+</style>
